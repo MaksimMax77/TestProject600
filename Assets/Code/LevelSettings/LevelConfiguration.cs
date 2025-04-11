5a1064f724016ca7537624acbcd961f130d307c7
m_krulov <mk@digi-tech.dev>
2025-04-11 20:04:58 +0300

    Комит добавляет юнити проект с кодом, получающим данные из RemoteConfigService.

---
+++ Assets/Code/LevelSettings/LevelConfiguration.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Code.LevelSettings
+{
+    [Serializable]
+    public class LevelConfiguration
+    {
+        public string expectedValue;
+        public string[] clusters;
+    }
+}
---
+++ Assets/Code/LevelSettings/LevelsConfigsInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Code.LevelSettings
+{
+    [Serializable]
+    public class LevelsConfigsInfo
+    {
+        public List<LevelConfiguration> levelConfigurations;
+    }
+}
